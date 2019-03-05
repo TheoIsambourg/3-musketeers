4ce4d5cd8bb60fa3d351177f4b7de633b7d8840d
TheoIsambourg <theo.isambourg@gmail.com>
2019-03-05 14:20:01 +0100

    finish CDD and TDD

--- cash/bin/cash.js
+++ cash/bin/cash.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/** Define the constants to use libraries*/
 const got = require('got');
 const money = require('money');
 const chalk = require('chalk');
@@ -8,10 +9,12 @@ const currencies = require('../lib/currencies.json');
 
 const {API} = require('./constants');
 
+
 const cash = async command => {
 	const {amount} = command;
 	const from = command.from.toUpperCase();
 	const to = command.to.filter(item => item !== from).map(item => item.toUpperCase());
+
 
 	console.log();
 	const loading = ora({
@@ -23,14 +26,31 @@ const cash = async command => {
 		}
 	});
 
+
 	loading.start();
 
+
+	/**
+	 * Call the API
+	 * 
+	 * @function
+	 * @this {got} 
+	 * @param {API} API to get the value
+	 */
 	await got(API, {
 		json: true
 	}).then(response => {
 		money.base = response.body.base;
 		money.rates = response.body.rates;
 
+
+		/**
+		 * Convert in the different currencies
+		 
+		 * @deprecated
+		 * @this {forEach}
+		 * @param {item} money is the amount
+		 */
 		to.forEach(item => {
 			if (currencies[item]) {
 				loading.succeed(`${chalk.green(money.convert(amount, {from, to: item}).toFixed(3))} ${`(${item})`} ${currencies[item]}`);
@@ -39,6 +59,7 @@ const cash = async command => {
 			}
 		});
 
+		/** In case of error */
 		console.log(chalk.underline.gray(`\nConversion of ${chalk.bold(from)} ${chalk.bold(amount)}`));
 	}).catch(error => {
 		if (error.code === 'ENOTFOUND') {
@@ -47,6 +68,8 @@ const cash = async command => {
 			loading.fail(chalk.red(`Internal server error :(\n${error}`));
 		}
 		process.exit(1);
+
+
 	});
 };
 
